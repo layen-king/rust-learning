fc3ae0b4b069fff44ea39bee106a1b91f9ae12aa
layen-king <446495695@qq.com>
2021-06-22 16:57:35 +0800

    添加枚举

--- src/utils.rs
+++ src/utils.rs
@@ -3,31 +3,49 @@ use std::collections::HashMap;
 use std::io::{Read, Result};
 use std::net::TcpStream;
 
-#[allow(dead_code)]
 #[derive(Debug)]
-pub struct Request<'a> {
-    method: &'a str,
-    url: &'a str,
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    OPTIONS,
+    NULL,
 }
 
-impl Request<'_> {
-    fn new<'a>(method: &'a str, url: &'a str) -> Request<'a> {
-        Request { method, url }
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Request {
+    method: Method,
+    url: String,
+    params: HashMap<String, String>,
+}
+
+impl Request {
+    fn new(method: Method, url: String, params: HashMap<String, String>) -> Request {
+        Request {
+            method,
+            url,
+            params,
+        }
     }
 }
 
-fn parse_url(requst: &str) -> Vec<&str> {
+/// 解析url成一个结构体
+fn parse_url(requst: &str) -> Request {
     let v: Vec<&str> = requst.split("\r\n").collect();
-    let mut map: HashMap<&str, &str> = HashMap::new();
+    let mut map: HashMap<String, String> = HashMap::new();
     for str in v.iter() {
         if str.contains(":") {
             let str = *str;
             let kv = str.split(":").collect::<Vec<&str>>();
-            map.insert(kv[0], kv[1]);
+            map.insert(kv[0].to_owned(), kv[1].to_owned());
         }
     }
-    println!("map: {:?}", map);
-    v
+    println!("v[0] {:?}", v[0]);
+    let req = v[0].split_whitespace().collect::<Vec<&str>>();
+    let req = Request::new(req[0].to_string() as Method, req[1].to_string(), map);
+    req
 }
 
 /// 处理tcp流
@@ -35,8 +53,7 @@ pub fn handle_connect(mut stream: TcpStream) -> Result<()> {
     let mut buf = [0; 1024];
     stream.read(&mut buf)?;
     let request = String::from_utf8_lossy(&buf);
-    let v = parse_url(&request);
-    let request = Request::new(v[0], v[1]);
-    println!("{:?}", request);
+    let r = parse_url(&request);
+    println!("{:?}", r);
     Ok(())
 }
