51225125431156fe97413421bab3a9e129a45a8e
layen <446495695@qq.com>
2022-03-09 16:57:37 +0800

    fix

--- del-repeat/src/repeat.rs
+++ del-repeat/src/repeat.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{Error, Write};
 use walkdir::WalkDir;
 
-pub fn find_repeat_file(path:String) -> Result<(), Error> {
+pub fn find_repeat_file(path: String) -> Result<(), Error> {
     let mut file_map = BTreeMap::new();
 
     for entry in WalkDir::new(&path)
@@ -15,18 +15,18 @@ pub fn find_repeat_file(path:String) -> Result<(), Error> {
         let file_path = entry.path().display().to_string();
         // 若存在同样的大小,push path到map
         if file_map.get(&file_size).is_none() {
-          let tem = vec![file_path];
-          file_map.insert(file_size, tem);
-        }else{
-          let tem = file_map.get_mut(&file_size).unwrap();
-          tem.push(file_path);
+            let tem = vec![file_path];
+            file_map.insert(file_size, tem);
+        } else {
+            let tem = file_map.get_mut(&file_size).unwrap();
+            tem.push(file_path);
         }
     }
     // 由小到大排序
     let path = format!("{}/result.json", &path);
     let mut output = File::create(path)?;
-    let str = String::from(format!("{:?}",file_map));
-    write!(output,"{}", str)?;
+    let str = String::from(format!("{:?}", file_map));
+    write!(output, "{}", str)?;
     Ok(())
 }
 
