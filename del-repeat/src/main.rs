c119adc1019c640fb039de0255ac33e6918713a0
layen <446495695@qq.com>
2022-03-22 15:37:33 +0800

    调增删除逻辑

--- del-repeat/src/main.rs
+++ del-repeat/src/main.rs
@@ -8,23 +8,24 @@ fn main() {
     let dir = std::env::current_dir().unwrap();
     let path = Path::new(&dir);
     loop {
-        let has_reapet = repeat::find_repeat_file((&path).to_path_buf());
+        println!("清输入要查询的文件地址");
+        let mut input_path = String::new();
+        stdout().flush().expect("input file path");
+        stdin().read_line(&mut input_path).expect("file path error");
+
+        let has_reapet = repeat::find_repeat_file(path.join(input_path.trim()));
         match has_reapet {
-            Ok((reapet,result)) => {
+            Ok((reapet, result)) => {
                 println!("发现重复文件,已将文件列表写入 result.json");
                 if reapet {
-                    println!(
-                        "是否进行删除操作? 输入Y/y/1,进行删除. 输入N/n取消删除,退出进程"
-                    );
+                    println!("是否进行删除操作? 输入Y/y/1,进行删除. 输入N/n取消删除,退出进程");
                     stdout().flush().expect("err");
                     let mut confirm = String::new();
                     stdin().read_line(&mut confirm).expect("输入错误");
                     match confirm.trim() {
                         "Y" | "y" | "1" => {
-                            let del = repeat::delete_repeat_file(result);
-                            if del.is_ok(){
-                                println!("删除重复文件成功,即将退出");
-                            }
+                            repeat::delete_repeat_file(result);
+                            println!("删除重复文件成功,即将退出");
                         }
                         _ => {
                             println!("不执行删除操作,进程即将退出");
@@ -39,13 +40,5 @@ fn main() {
                 println!("错误:{:?}", err)
             }
         }
-
-        // let args:Vec<String> = env::args().collect();
-        // let mut path = String::from("./");
-        // if !args.get(1).is_none() {
-        //   path = args.get(1).unwrap().to_string();
-        // }
-        // let result = repeat::find_repeat_file(path);
-        // println!("{:?}",result);
     }
 }
--- del-repeat/src/repeat.rs
+++ del-repeat/src/repeat.rs
@@ -1,13 +1,13 @@
 use std::collections::BTreeMap;
-use std::fs::File;
+use std::fs::{remove_file, File};
 use std::io::{Error, Write};
-use walkdir::WalkDir;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 /// ## 查找重复文件并且输出
 /// ### [path] 要查询的文件路径
-pub fn find_repeat_file(path: PathBuf) -> Result<(bool,BTreeMap<u64,Vec<String>>), Error> {
-    println!("读取路径文件:{:?}",path);
+pub fn find_repeat_file(path: PathBuf) -> Result<(bool, BTreeMap<u64, Vec<String>>), Error> {
+    println!("读取路径文件:{:?}", path);
     let mut file_map = BTreeMap::new();
 
     let mut has_reapet = false;
@@ -20,7 +20,7 @@ pub fn find_repeat_file(path: PathBuf) -> Result<(bool,BTreeMap<u64,Vec<String>>
         let file_size = entry.metadata()?.len();
         let file_path = entry.path().display().to_string();
         // 若存在同样的大小,push path到map
-        if file_map.get(&file_size).is_none() {
+        if !file_map.contains_key(&file_size) {
             let tem = vec![file_path];
             file_map.insert(file_size, tem);
         } else {
@@ -33,27 +33,37 @@ pub fn find_repeat_file(path: PathBuf) -> Result<(bool,BTreeMap<u64,Vec<String>>
     let mut output = File::create(path)?;
     let str = String::from(format!("{:?}", file_map));
     write!(output, "{}", str)?;
-    Ok((has_reapet,file_map))
+    Ok((has_reapet, file_map))
 }
 
 /// ## 删除重复文件
 /// ### [result] 要删除的文件,以BTreeMap保存
-pub fn delete_repeat_file(mut result:BTreeMap<u64,Vec<String>>)-> Result<bool,Error>{
-    println!("{:#?}",result);
-    result.iter_mut().for_each(|res|{
-        if res.1.len() > 1{
-            res.1.iter_mut().for_each(|path|{
-                println!("删除文件:{}",path);
-            })
+pub fn delete_repeat_file(mut result: BTreeMap<u64, Vec<String>>) {
+    println!("{:#?}", result);
+    let mut result_iter = result.iter_mut();
+    'file: loop {
+        if let Some((_size,path_list)) = result_iter.next(){
+            if path_list.len() > 1 {
+                let mut iter = path_list.iter_mut().skip(1);
+                'del: loop{
+                    if let Some(path) = iter.next(){
+                        println!("delete {:?}", path);
+                        let _ = remove_file(path);
+                    }else{
+                        break 'del;
+                    }
+                }
+            }
+        }else { 
+            break 'file;
         }
-    });
-    todo!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
     use super::find_repeat_file;
+    use std::path::Path;
     #[test]
     fn test() {
         let res = find_repeat_file(Path::new("../").to_path_buf());
