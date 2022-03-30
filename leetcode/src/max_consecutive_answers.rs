eedb5b0ed75d37ddcef140738c05b94221604e81
layen-king <446495695@qq.com>
2022-03-30 20:12:31 +0800

    fix: 解决考试的最大困扰度

--- leetcode/src/max_consecutive_answers.rs
+++ leetcode/src/max_consecutive_answers.rs
@@ -4,9 +4,9 @@
 
 /// ### 一位老师正在出一场由 n 道判断题构成的考试，每道题的答案为 true （用 'T' 表示）或者 false （用 'F' 表示）。老师想增加学生对自己做出答案的不确定性，方法是 最大化 有 连续相同 结果的题数。（也就是连续出现 true 或者连续出现 false）。
 
-/// ### 给你一个字符串 answerKey ，其中 answerKey[i] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
+/// ### 给你一个字符串 answerKey ，其中 answerKey\[i\] 是第 i 个问题的正确结果。除此以外，还给你一个整数 k ，表示你能进行以下操作的最多次数：
 
-/// ### 每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey[i] 改为 'T' 或者 'F' ）。
+/// ### 每次操作中，将问题的正确答案改为 'T' 或者 'F' （也就是将 answerKey\[i\] 改为 'T' 或者 'F' ）。
 /// ### 请你返回在不超过 k 次操作的情况下，最大 连续 'T' 或者 'F' 的数目。
 
 /// ###
@@ -46,27 +46,36 @@
 /// ### 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 pub fn max_consecutive_answers(answer_key: String, k: i32) -> i32 {
     let (mut ans, mut left, mut t, mut f) = (0, 0, 0, 0);
-    for (right, ch) in answer_key.chars().enumerate() {
-        if ch == 'T' {
-            t = t + 1;
-        } else {
-            f = f + 1;
+    let chars: Vec<_> = answer_key.chars().collect();
+    for (right, ch) in chars.iter().enumerate() {
+        match *ch {
+            'T' => {
+                t = t + 1;
+            }
+            'F' => {
+                f = f + 1;
+            }
+            _ => {}
         }
-        while t > k && f > k {
-            if answer_key.chars().nth(left) == Some('T') {
-                t = t - 1;
-            } else {
-                f = f - 1;
+        if t > k && f > k {
+            match chars.get(left) {
+                Some(&'T') => {
+                    t = t - 1;
+                }
+                Some(&'F') => {
+                    f = f - 1;
+                }
+                _ => {}
             }
             left = left + 1;
         }
-        ans = ans.max(right - left + 1)
+        ans = ans.max(right - left + 1);
     }
     ans as i32
 }
 
 #[test]
 fn test_max_consecutive_answers() {
-    let result = max_consecutive_answers(String::from("TTTTTFTT"), 1);
-    assert_eq!(result, 51)
+    let result = max_consecutive_answers(String::from("TFTTTFTT"), 1);
+    assert_eq!(result, 6)
 }
