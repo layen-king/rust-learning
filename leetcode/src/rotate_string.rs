0ced79a10b432a1e391d1a0652b97939929e78e8
layen-king <446495695@qq.com>
2022-04-07 09:36:51 +0800

    feat: 增加工作区配置 feat：旋转字符串

---
+++ leetcode/src/rotate_string.rs
@@ -0,0 +1,37 @@
+/// ## 旋转字符串
+
+/// ### 简单
+
+/// ### 给定两个字符串, s 和 goal。如果在若干次旋转操作之后，s 能变成 goal ，那么返回 true 。
+
+/// ### s 的 旋转操作 就是将 s 最左边的字符移动到最右边。 
+
+/// ### 例如, 若 s = 'abcde'，在旋转一次之后结果就是'bcdea' 。
+/// ###  
+
+/// ### 示例 1:
+
+/// ### 输入: s = "abcde", goal = "cdeab"
+/// ### 输出: true
+/// ### 示例 2:
+
+/// ### 输入: s = "abcde", goal = "abced"
+/// ### 输出: false
+/// ###  
+
+/// ### 提示:
+
+/// ### 1 <= s.length, goal.length <= 100
+/// ### s 和 goal 由小写英文字母组成
+/// ### 通过次数33,066提交次数58,431
+
+/// ### 来源：力扣（LeetCode）
+/// ### 链接：https://leetcode-cn.com/problems/rotate-string
+/// ### 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+pub fn rotate_string(s: String, goal: String) -> bool {
+    // 若长度不相等，直接返回false
+    if s.len() != goal.len() { return false; }
+    let mut str = s.clone();
+    str.push_str(&s);
+    str.contains(&goal)
+}
