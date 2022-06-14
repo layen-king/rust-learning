4767b00e5041b0f1ec47fe120bf093248bbfdeeb
layen-king <446495695@qq.com>
2022-06-14 09:50:47 +0800

    feat: task

--- void/rust_course/src/async_future_excuting.rs
+++ void/rust_course/src/async_future_excuting.rs
@@ -1,11 +1,16 @@
 use std::{
     future::Future,
     pin::Pin,
-    sync::{Arc, Mutex},
+    sync::{
+        mpsc::{sync_channel, Receiver, SyncSender},
+        Arc, Mutex,
+    },
     task::{Context, Poll, Waker},
     thread,
     time::Duration,
 };
+
+use futures::future::BoxFuture;
 
 pub struct TimerFuture {
     shared_state: Arc<Mutex<SharedState>>,
@@ -58,4 +63,35 @@ impl TimerFuture {
 
         TimerFuture { shared_state }
     }
+}
+/// 执行任务
+struct Task {
+    /// 进行中的Future，在未来的某个时间点会被完成
+    ///
+    /// 按理来说`Mutex`在这里是多余的，因为我们只有一个线程来执行任务。但是由于
+    /// Rust并不聪明，它无法知道`Future`只会在一个线程内被修改，并不会被跨线程修改。因此
+    /// 我们需要使用`Mutex`来满足这个笨笨的编译器对线程安全的执着。
+    ///Future
+    /// 如果是生产级的执行器实现，不会使用`Mutex`，因为会带来性能上的开销，取而代之的是使用`UnsafeCell`
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    /// 可以将该任务自身放回到任务通道中，等待执行器的poll
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+// 任务执行器
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+fn new_executor_and_spawn() -> (Executor, Spawner) {
+    // 任务通道允许的最大缓冲数(任务队列的最大长度)
+    // 当前的实现仅仅是为了简单，在实际的执行中，并不会这么使用
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+    (Executor { ready_queue }, Spawner { task_sender })
 }
